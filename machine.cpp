821df4215a8d1951c17545e944ee8237c378492f
IgorMelnikov1 <melnikov.ia@phystech.edu>
2020-11-04 14:33:02 +0300

    attempt

--- machine.cpp
+++ machine.cpp
@@ -1,5 +1,10 @@
+#ifndef MACHINE_CPP
+#define MACHINE_CPP
+
 #include "machine.h"
 
 std::pair<bool, bool>& CInfo::operator [](size_t i) {
-        return info[i];
-    }
+    return info[i];
+}
+
+#endif
